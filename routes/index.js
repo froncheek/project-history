b7a77a1b9789f34cf24168b21b350b254913470c
froncheek <jerwin.ilagan@gmail.com>
2015-10-10 17:39:10 +0800

    Realigning the tab spaces. Not much of a changes.

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,12 @@ var leya = require('jsleya');
 leya.create('leya.Route', {
 	module: module,
 	invoke: function(reply) {
-        //leya.create('leya.view.MasterPage', {
-    	leya.create('App.view.Index', {
-    		reply: reply,
-
-    		complete: function(err) {
-    			reply.send(this.getHtml());
-    		}
-    	});
+		leya.create('App.view.Index', {
+			reply: reply,
+		
+			complete: function(err) {
+				reply.send(this.getHtml());
+			}
+		});
 	}
 });
